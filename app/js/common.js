68a49d675329715b0b52b5e266065ebd306678b6
AegisFair <geforcee1995@gmail.com>
2019-06-23 19:02:54 +0300

    owl-carusel done

--- app/js/common.js
+++ app/js/common.js
@@ -19,12 +19,24 @@ $(function(){
     }).bind('closed', function(){
         $('.hamburger').removeClass('is-active');
     })
+    
+    /*$('.carousel-services').on('initialized.owl.carousel', function(){
+        setTimeout(
+            function(){
+                carouselService();
+            },100);
+    })*/
+    $('.carousel-services-composition h3').each(function(){
+        $(this).html($(this).html().replace(/(\S+)\s*$/,'<span>$1</span>'))
+    })
+
     $('.carousel-services').owlCarousel({
-        loop: true,
+        loop: false,
         nav: true,
         smartSpeed: 700,
         navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
         responsiveClass: true,
+        dots: false,
         responsive: {
             0: {
                 items: 1
@@ -38,4 +50,18 @@ $(function(){
         }
 
     });
+
+    function carouselService(){
+        $('.carousel-services-item').each(function (){
+                var contentHeight = $(this).find('.carousel-services-content').outerHeight();
+                $(this).find('.carousel-services-image').css('min-height',contentHeight);
+        })
+    };
+    
+    // resize Window
+    function onResize(){
+        $(".carousel-services-content").equalHeights();
+        carouselService();
+    }onResize();
+    window.onresize = onResize;
 });
